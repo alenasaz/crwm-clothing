4c6f4a7935b2dd299a4b34e5bc47421f2c1bcdef
alenasaz <aLeNka_saZaNova@mail.ru>
2021-10-02 19:35:51 +0300

    Added sign-in and sign-up component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import FormInput from '../form-input/form-input.component';
 import './sign-in.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
 
-import {signInWithGoogle} from  '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from  '../../firebase/firebase.utils';
 
 class SignIn extends React.Component 
 {
@@ -17,10 +17,17 @@ class SignIn extends React.Component
         
     }
 
-    handleSubmit = event =>
+    handleSubmit = async event =>
     {
         event.preventDefault();
-        this.setState( {email: '', password: ''})
+        const { email, password } = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState( {email: '', password: ''});
+        } catch (error) {
+            console.log(error);
+        }
+        
     }
 
     handleChange = event =>
@@ -39,7 +46,7 @@ class SignIn extends React.Component
         <FormInput name="email" type="email" label="email"
         handleChange={this.handleChange} value={this.state.email} required />
         <FormInput name="password" type="password" label="password" 
-        handleChange={this.handleChange} value={this.state.email} required />
+        handleChange={this.handleChange} value={this.state.password} required />
         <div className='buttons'>
         <CustomButton type="submit" > Sign In</CustomButton>
         <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign In with Google</CustomButton>
--- src/pages/sign-in-up/sign-in-up.component.jsx
+++ src/pages/sign-in-up/sign-in-up.component.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './sign-in-up.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInUp = () =>
 (
     <div className='sign-in-up'>
     <SignIn> </SignIn>
+    <SignUp></SignUp>
     </div>
 );
 
